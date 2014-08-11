40bbcc3d0e5045845076c6640d2aa358ff33c0d4
DeanStuff <weed101@gmail.com>
2014-08-10 20:33:36 -0400

    removing unecessary classes

--- src/main/java/com/jt/Properties.java
+++
@@ -1,9 +0,0 @@
-package com.jt;
-
-public class Properties {
-
-    public static String OUTPUT_PATH = "jt.output.path";
-    public static String STORE_UNZIPPED = "jt.store.unzipped";
-    public static String REST_ENDPOINT = "jt.input.uri";   
-    public static String PRESERVE_PATH = "jt.preserve.filepath";
-}
